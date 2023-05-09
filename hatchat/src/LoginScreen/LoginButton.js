25ff15f25de34f8c8f6b5b539313e0212dc63854
Yuval Arbel <Arbelyuval56@gmail.com>
2023-05-09 21:37:33 +0300

    new database + new logic for login button that check if he is in the data base

--- hatchat/src/LoginScreen/LoginButton.js
+++ hatchat/src/LoginScreen/LoginButton.js
@@ -1,12 +1,22 @@
-import { Link } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import {users} from '../DataBase/Database';
 
+function LoginButton({username, password}) {
+    const navigate = useNavigate();
 
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const user = users.find((user) => user.username === username && user.password === password);
+        if (user) {
+            navigate('/chat');
+        } else {
+            alert('Invalid username or password');
+        }
+    };
 
-function LoginButton() {
     return (
-        <div>
-            <a className="btn btn-large btn-primary" href="ChatScreen.html">Log In</a>
-        </div>
+        <button type="submit" className="btn btn-large btn-primary" onClick={handleLogin}>Log In</button>
     );
 }
+
 export default LoginButton;
