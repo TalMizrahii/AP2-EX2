424646610295ccc77621e3b063a6061e0e61caa9
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2023-05-12 17:12:49 +0300

    Adding new message, starting to add to a specific user.

--- hatchat/src/ChatScreen/ChatConversation/MsgWrapperScroll.js
+++ hatchat/src/ChatScreen/ChatConversation/MsgWrapperScroll.js
@@ -1,6 +1,28 @@
-function MsgWrapperScroll({children}) {
+import { useEffect, useRef } from 'react';
+
+function MsgWrapperScroll({ children }) {
+    const chatBoxRef = useRef(null);
+
+    useEffect(() => {
+        const chatBox = chatBoxRef.current;
+
+        // Scroll to the bottom of the chat box initially
+        chatBox.scrollTop = chatBox.scrollHeight;
+
+        // Scroll to the bottom of the chat box on new messages
+        const observer = new MutationObserver(() => {
+            chatBox.scrollTop = chatBox.scrollHeight;
+        });
+        observer.observe(chatBox, { childList: true });
+
+        // Clean up the observer when component unmounts
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
     return (
-        <div className="DivWithScroll">
+        <div className="DivWithScroll" ref={chatBoxRef}>
             <div id="chatBoxOfMessages">
                 {children}
             </div>
