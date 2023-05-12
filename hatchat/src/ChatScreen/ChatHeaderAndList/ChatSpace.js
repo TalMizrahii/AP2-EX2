8417f7b7c8b375fa7990cebac3b5b31aadc66e3a
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2023-05-12 09:33:58 +0300

    Removed the Bootstrap import from index.js.

--- hatchat/src/ChatScreen/ChatHeaderAndList/ChatSpace.js
+++ hatchat/src/ChatScreen/ChatHeaderAndList/ChatSpace.js
@@ -10,7 +10,6 @@ import SearchInput from "./SearchInput";
 import ListGroupOfContacts from "./ListGroupOfContacts.";
 // import {useState} from "react";
 import contactsData from "../../DataBase/ContactsData";
-
 import React, {useState} from "react";
 
 // import contactsData from "../../DataBase/ContactsData";
@@ -62,60 +61,3 @@ function ChatSpace() {
 }
 
 export default ChatSpace;
-
-
-//
-//
-//
-// import HeaderBox from "./HeaderBox";
-// import UserImageLeftTopCorner from "./UserImageLeftTopCorner";
-// import NavIcons from "./NavIcons";
-// import AddContactIcon from "./AddContactIcon";
-// import ModalAddContactWrap from "./ModalAddContactWrap";
-// import ModalAddContact from "./ModalAddContact";
-// import ManuWrapper from "./ManuWraper";
-// import DropItemListBtns from "./DropItemListBtns";
-// import SearchInput from "./SearchInput";
-// import ListGroupOfContacts from "./ListGroupOfContacts.";
-// import {useState} from "react";
-// import contactsData from "../../DataBase/ContactsData";
-//
-// function ChatSpace() {
-//
-//     const [filterList, setFilterList] = useState(contactsData);
-//
-//     const doSearch = function (content) {
-//         setFilterList(contactsData.filter((a) =>
-//             a.name.includes(content)
-//         ));
-//     }
-//     const addContact = function (contact) {
-//         setFilterList((prevList) => [...prevList, contact]);
-//     };
-//
-//     return (<>
-//             <div className=" col-md-3 g-0 chatScreen">
-//                 <HeaderBox>
-//                     <UserImageLeftTopCorner/>
-//                     <NavIcons>
-//                         <AddContactIcon/>
-//                         <ModalAddContactWrap>
-//                             <ModalAddContact addContact={addContact}/>
-//                         </ModalAddContactWrap>
-//                         <ManuWrapper>
-//                             <DropItemListBtns>Switch user</DropItemListBtns>
-//                             <DropItemListBtns>Change profile picture</DropItemListBtns>
-//                             <DropItemListBtns>Archive</DropItemListBtns>
-//                             <DropItemListBtns>Logout</DropItemListBtns>
-//                         </ManuWrapper>
-//                     </NavIcons>
-//                 </HeaderBox>
-//                 <SearchInput doSearch={doSearch}/>
-//                 <ListGroupOfContacts filter={filterList}/>
-//
-//             </div>
-//         </>
-//     );
-// }
-//
-// export default ChatSpace;
