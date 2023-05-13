b9d50fb6ac44093a05c94855be7f15b65e75f1ed
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2023-05-13 21:35:26 +0300

    fixed minor issues with the pictures.

--- hatchat/src/ChatScreen/ChatConversation/UserSelfMsg.js
+++ hatchat/src/ChatScreen/ChatConversation/UserSelfMsg.js
@@ -3,12 +3,7 @@ import React from "react";
 function UserSelfMsg({msg, currentContact}) {
 
     return (
-        <div className="chatText d-flex flex-row justify-content-start">
-            <img
-                className="chatImg"
-                src="https://images.squarespace-cdn.com/content/v1/5c76de607fdcb8facd765433/1592926322727-OL8OFAUGXH0Q5XMF6AXC/IMG-4874.JPG"
-                alt="avatar 1"
-            />
+        <div className="chatText d-flex flex-row justify-content-end">
             <div>
                 <p className="ContentMsg small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
                     {msg.text}
@@ -16,6 +11,11 @@ function UserSelfMsg({msg, currentContact}) {
                 <p className="small me-3 mb-3 rounded-3 text-muted float-end"> {msg.timeAndDate}
                 </p>
             </div>
+            <img
+                className="chatImg"
+                src="https://images.squarespace-cdn.com/content/v1/5c76de607fdcb8facd765433/1592926322727-OL8OFAUGXH0Q5XMF6AXC/IMG-4874.JPG"
+                alt="avatar 1"
+            />
         </div>
     );
 }
