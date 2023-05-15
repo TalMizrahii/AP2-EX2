dfae4b88f6e28debc52fe8e0e8da9b42d525b42b
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2023-05-15 20:30:33 +0300

    Trying to connect the chat messages to a user.

--- hatchat/src/LoginScreen/LoginButton.js
+++ hatchat/src/LoginScreen/LoginButton.js
@@ -1,31 +1,4 @@
-import {useNavigate} from 'react-router-dom';
-import {users} from '../DataBase/Database';
-
-function LoginButton({username, password}) {
-    console.log('Users Data:', users);
-
-    const navigate = useNavigate();
-
-    const handleLogin = (event) => {
-        event.preventDefault();
-        const user = users.find((user) => user.userName === username);
-        if (user) {
-            if (user.password === password) {
-
-                navigate('/chat', {
-                    state: {
-                        fullName: user.fullName, userName: user.userName,
-                        userPassword: user.password,
-                        profilePicture: user.profilePicture
-                    }
-                });
-            } else {
-                alert('Invalid password');
-            }
-        } else {
-            alert('Invalid username or password');
-        }
-    };
+function LoginButton({handleLogin}) {
 
     return (
         <button type="submit" className="btn btn-large btn-primary" onClick={handleLogin}>
